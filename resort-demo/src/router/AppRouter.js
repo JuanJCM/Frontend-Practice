60f6a48a8ec66de442ea598debb48b8d5fdc7a24
JuanJCM <50124087+JuanJCM@users.noreply.github.com>
2022-05-06 14:13:50 -0600

    Using Css
    making some examples using css

---
+++ resort-demo/src/router/AppRouter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "../components/Header";
+import AddBook from "../components/AddBook";
+import BookList from "../components/BookList";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const AppRouter = () => {
+    const[books, setBook] = useLocalStorage('books', []);
+    return(
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <div className="main-content">
+                    <Switch>
+                        <Route render={(props)=> (
+                            <BookList {...props} books ={books} setBooks = {setBooks}/>
+                        )} path= "/" exact = {true}/>
+                        <Route render ={(props) =>(
+                            <AddBook{...props} books={books} setBooks={setBooks}/>
+                        )} path ="/add"/>
+                    </Switch>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+};
+export default AppRouter;
---
+++ test-css/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class='T1'>Hello World</h1>
+      <h1 class='T2'>Hello World</h1>
+      <h1 class='T3'>Hello World</h1>
+      <h1 class='T4'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;

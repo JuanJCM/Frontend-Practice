2ea4a0d513a476b163a78a44544d56f0bde566e9
JuanJCM <50124087+JuanJCM@users.noreply.github.com>
2022-05-10 10:32:14 -0600

    sass example
    example using sass

---
+++ sass-example/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>This is a variable example</h1>
+
+    <div id='nested'>
+      <h3>
+        This is a nested rules example
+      </h3>
+    </div>
+    <p className='interpolation'>interpolation Example.</p>
+    <div id="function">
+      <h3>
+        This is a function example
+      </h3>
+    </div>
+    <div id='mixin'>
+      <p>This is mixin Example</p>
+    </div>
+  </>
+  );
+}
+
+export default App;

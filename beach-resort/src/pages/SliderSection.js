f6b1964a859e71a0025460a8e356bcf08a239ed1
Juan Carranza <j.carranza@unitec.edu>
2022-05-19 15:36:47 -0600

    creating slider section

---
+++ beach-resort/src/pages/SliderSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SliderSection=()=>{
+return(
+    <div className="sliderSection">
+    <h1>Aqui deberia de ir un slider, si tan solo tuviera uno</h1>
+</div>
+);
+}
+export default SliderSection;

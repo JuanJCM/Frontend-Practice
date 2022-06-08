a05401576cde5ebaaf10ee09b9dcae2671f74da8
JuanJCM <50124087+JuanJCM@users.noreply.github.com>
2022-06-08 09:00:09 -0600

    Tuesday finish up changing to mobile

--- beach-resort/src/pages/EndPage.js
+++ beach-resort/src/pages/EndPage.js
@@ -4,7 +4,7 @@ const EndPage = () => {
   return (
     <div className="EndPage">
       <img className="bluelogo" src={logo} alt="logo" />
-      <h2>More Quality Inclusions</h2>
+      <h2 className="moretext">More Quality Inclusions</h2>
       <h3 className="useOpenstext">THAN ANY OTHER RESORTS ON THE PLANET</h3>
       <h3 className="viewButton">View All Inclusions</h3>
     </div>

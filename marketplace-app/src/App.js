2a8629656f59a4c57eeffc6dff088f5693dfdf66
JuanJCM <50124087+JuanJCM@users.noreply.github.com>
2022-06-14 16:08:50 -0600

    Init Marketplace app

---
+++ marketplace-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import HeaderComponent from './components/HeaderComponent';
+
+function App() {
+  return (
+    <div className="body">
+      <HeaderComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ marketplace-app/src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+const HeaderComponent=()=>{
+    return(
+        <div className="Header">
+        <div className="placeholder">
+        <h1>Hola mundo</h1>
+        </div>
+    </div>
+
+    );
+}
+export default HeaderComponent;
